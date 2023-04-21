09b8ae56de07997366c693497abdfdd284ae66b7
Jacob Hein <jacobhein1892@gmail.com>
2023-04-20 20:04:59 -0700

    working on figuring out pointers

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 // Build a simple text-based contact management system that allows users to add, edit, delete, and search for contacts using a linked list
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
@@ -13,20 +14,43 @@ typedef struct node {
 
 int main(void){
     bool newContact = true; 
-    while(newContact == true){
-        contact first_contact;
-        char name[50];
-        char addMore[1];
-        printf("Please Enter your first contact's name: ");
-        fgets(name, sizeof(name), stdin);
-        strcpy(first_contact.name, name);
-        printf("Name: %s\n", first_contact.name);
-        printf("Would you like to add another contact?(y/n) ");
-        scanf("%c", addMore);
+    contact* currptr; // Start with a first pointer.
 
-        if (strcmp(addMore, "n\n") == 0){
-            newContact = false;
+    while(newContact == true){
+        currptr = (contact*) malloc((sizeof(contact))); // Allocate memory for a new contact
+        if (currptr == NULL){
+            printf("Cannot allocate memory!");
+            exit(0);
         }
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+        // contact first_contact;
+        // char name[50];
+        // char addMore[1];
+        // printf("Please Enter your first contact's name: ");
+        // fgets(name, sizeof(name), stdin);
+        // strcpy(first_contact.name, name);
+        // printf("Name: %s\n", first_contact.name);
+        // printf("Would you like to add another contact?(y/n) ");
+        // scanf("%c", addMore);
+
+        // if (strcmp(addMore, "n\n") == 0){
+        //     newContact = false;
+        // } else {
+
+        // }
     } 
 
 
