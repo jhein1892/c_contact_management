38ca20f240eddab7f5be6576cff19aa35baa4e24
Jacob Hein <jacobhein1892@gmail.com>
2023-04-20 18:41:17 -0700

    working on building ability to add multiple contacts

---
+++ main.c
@@ -0,0 +1,35 @@
+// Build a simple text-based contact management system that allows users to add, edit, delete, and search for contacts using a linked list
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct node {
+    char name[50];
+    char phone[20];
+    char email[40];
+    struct node * next; // creating a pointer called next
+} contact;
+
+int main(void){
+    bool newContact = true; 
+    while(newContact == true){
+        contact first_contact;
+        char name[50];
+        char addMore[1];
+        printf("Please Enter your first contact's name: ");
+        fgets(name, sizeof(name), stdin);
+        strcpy(first_contact.name, name);
+        printf("Name: %s\n", first_contact.name);
+        printf("Would you like to add another contact?(y/n) ");
+        scanf("%c", addMore);
+
+        if (strcmp(addMore, "n\n") == 0){
+            newContact = false;
+        }
+    } 
+
+
+    printf("Thank you for adding contacts!");
+    return 0; 
+}
