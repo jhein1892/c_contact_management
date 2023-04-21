814b1da90d30a08a3fe14eccbb9054083612e3e9
Jacob Hein <jacobhein1892@gmail.com>
2023-04-21 12:15:14 -0700

    dynamically allocating memory for a single contact

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 // Build a simple text-based contact management system that allows users to add, edit, delete, and search for contacts using a linked list
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,44 +12,23 @@ typedef struct node {
 } contact;
 
 int main(void){
-    bool newContact = true; 
+    bool addContact = true; 
     contact* currptr; // Start with a first pointer.
 
-    while(newContact == true){
+    while(addContact == true){
         currptr = (contact*) malloc((sizeof(contact))); // Allocate memory for a new contact
         if (currptr == NULL){
             printf("Cannot allocate memory!");
             exit(0);
         }
-        
-        
-        
+        strcpy(currptr->name, "Jake Hein");
+        strcpy(currptr->email, "email@email.com");
+        strcpy(currptr->phone, "100-xxx-xxxx");
+        currptr->next = NULL;
 
 
-
-
-
-
-
-
-
-
-
-        // contact first_contact;
-        // char name[50];
-        // char addMore[1];
-        // printf("Please Enter your first contact's name: ");
-        // fgets(name, sizeof(name), stdin);
-        // strcpy(first_contact.name, name);
-        // printf("Name: %s\n", first_contact.name);
-        // printf("Would you like to add another contact?(y/n) ");
-        // scanf("%c", addMore);
-
-        // if (strcmp(addMore, "n\n") == 0){
-        //     newContact = false;
-        // } else {
-
-        // }
+        printf("Name: %s\nEmail: %s\nPhone: %s\n", currptr->name, currptr->email, currptr->phone);
+        addContact = false;
     } 
 
 
